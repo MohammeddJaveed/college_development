fc778706123ecf2b9fafd04561be0a76357f754a
Mohammed Javeed <Mdjaveed9108@gmail.com>
2024-09-27 22:27:02 +0100

    Sum of multiples

--- sumOfMultiples.js
+++ sumOfMultiples.js
@@ -1,1 +1,12 @@
-
+function sumOfMultiples(a,b,n){
+  let sum=0;
+  for(let i=1;i<n;i++){
+    if(a%i==0 || b%i==0){
+      sum += i;
+    }
+  }
+  return sum
+} 
+const result= sumOfMultiples(a,b){
+  alert (result)
+}
