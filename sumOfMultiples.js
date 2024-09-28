6bbd5303eb360f6ae6a91b0d7060ab08855ed4f4
Mohammed Javeed <Mdjaveed9108@gmail.com>
2024-09-28 12:12:56 +0100

    changes

--- sumOfMultiples.js
+++ sumOfMultiples.js
@@ -1,6 +1,6 @@
 function sum(a,b,n)
     {
-        s=0;
+       let  s=0;
         for(i=1;i<n;i++)
         {
             if(i%a==0 || i%b==0)
@@ -15,4 +15,5 @@ function GetValues()
     num2 = document.getElementById("num2").value;
     limit = document.getElementById("limit").value;
     sum(num1,num2,limit);
+    alert(sum)
 }
