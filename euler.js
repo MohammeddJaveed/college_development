6e1c10206e4ec50184602b5373b62f39baa1d994
Mohammed Javeed <Mdjaveed9108@gmail.com>
2024-10-03 13:39:03 +0100

    Dom elemets added

--- euler.js
+++ euler.js
@@ -12,10 +12,32 @@ function sum(a,b,n)
 
 function GetValues()
 {
-   document.getElementById("Intro").innerHTML = "This is a page to calculate the sum of multiples of certain range"
+  
     num1 = document.getElementById("num1").value;
     num2 = document.getElementById("num2").value;
     limit = document.getElementById("limit").value;
     sum(num1,num2,limit);
+
+}
+function resetValues()
+{
+    document.getElementById("num1")="";
+    document.getElementById("num2")="";
+    document.getElementById("limit")="";
+
+}
+function applyStyles(){
+    const mainHead = document.getElementById("mainHead");
+    mainHead.style.color="#008000"
+    mainHead.style.alignContent ="center"
+    mainHead.style.fontSize="15px"
+
+    const checkSum = document.getElementById("checkSum");
+    checkSum.style.backgroundColor = "#008080"
+     checkSum.style.fontFamily ="Arial, sans-serif"
+
+    const clear = document.getElementById("clear");
+    clear.style.backgroundColor = "#008080"
+    clear.style.fontFamily ="Arial, sans-serif"
 
 }
